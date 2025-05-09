e7c58b1a3cd76b7d2d70c4991069ec7765fef808
coin-cyb <lantylqt@gmail.com>
2025-05-09 15:41:24 +0800

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GoalManager from './GoalManager';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<GoalManager />);
